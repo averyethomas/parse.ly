05b4a3b38186b1223bba26ad18e44df55d6bf653
Avery Thomas <averyethomas@gmail.com>
2018-04-08 18:48:39 -0400

    getting top posts

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('angularApp', []);
+
+app.controller('parselyCtrl', ['$scope', '$http', function ($scope, $http) {
+    
+    var apiKey = 'texasmonthly.com';
+    var accessToken = 'J44znyy0vsL7tsYzacxIjtVZhQA4uT8kQqN2ZK72Sko';
+
+    var URL = 'https://api.parsely.com/v2/analytics/posts?apikey=' + apiKey +'&secret=' + accessToken + '&page=1&limit=10&sort=views&period_start=24h'
+    $scope.topPosts = [];
+
+    $scope.getPosts = function(){
+        $http({
+            method: 'GET',
+            url: URL,
+        }).
+            success(function(data, status, headers, config){
+                $scope.topPosts = data.data;
+                console.log($scope.topPosts);
+        }).
+            error(function(data, status, headers, config){
+                console.log('error');
+            });
+    };
+    
+    $scope.getPosts();
+    
+}]);
